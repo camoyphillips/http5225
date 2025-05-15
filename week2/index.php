aaf98b42867abe8de64c402b13a99244e1a8591d
Camoy Phillips <n01496384@humber.ca>
2025-05-14 20:24:05 -0400

    Learning php syntax

---
+++ week2/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week2</title>
+</head>
+<body>
+    <h1>Hello Hello</h1>
+
+    <?php
+
+    $firstname="Camoy";
+    $lastname="Phillips";
+
+
+
+    echo = $firstname . $lastname . "";
+
+
+
+
+
+?>
+    
+</body>
+</html>
